7b9a9d194cb647ba3f5218a0af100515b30b5b1f
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-08-04 23:22:27 +0530

    Use `ImageResponse` in templates.rs

--- src/routes/image/templates.rs
+++ src/routes/image/templates.rs
@@ -1,7 +1,8 @@
 use rocket::State;
 
 use crate::{
-    datastructures::template::TemplateInput, errors::Errors, state::serverstate::ServerState,
+    datastructures::template::TemplateInput, errors::Errors,
+    imagelib::image_response::ImageResponse, state::serverstate::ServerState,
 };
 
 #[post("/<name>", data = "<template_input>")]
@@ -9,13 +10,10 @@ pub async fn template(
     name: String,
     server_state: &State<&'static ServerState>,
     template_input: TemplateInput<'_>,
-) -> Result<Vec<u8>, Errors> {
+) -> Result<ImageResponse, Errors> {
     let template_input = template_input?.into_inner();
     let template = server_state.config.get_template(name)?;
     template.validate(&template_input)?;
     let image = template.process(server_state, template_input).await?;
-
-    let mut bytes: Vec<u8> = Vec::new();
-    image.write_to(&mut bytes, image::ImageOutputFormat::Png)?;
-    Ok(bytes)
+    ImageResponse(image).ok()
 }
